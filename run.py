ca7b335d9d24769315032ec8547ab4636c9f061e
Yohan <yohan.yudhistira90@gmail.com>
2021-05-03 17:46:33 +0700

    download and save image

--- run.py
+++ run.py
@@ -3,7 +3,7 @@ from bs4 import BeautifulSoup
 import os
 
 
-def imagedown(url, folder):
+def image_down(url, folder):
     try:
         os.mkdir(os.path.join(os.getcwd(), folder))
     except:
@@ -19,4 +19,4 @@ def imagedown(url, folder):
             im = requests.get(link)
             f.write(im.content)
             print('Writing: ', name)
-imagedown('https://www.airbnb.co.uk/s/Ginza--Chuo--Tokyo--Japan/homes?tab_id=home_tab&refinement_paths%5B%5D=%2Fhomes&flexible_trip_dates%5B%5D=june&flexible_trip_dates%5B%5D=may&flexible_trip_lengths%5B%5D=weekend_trip&date_picker_type=calendar&query=Ginza%2C%20Chuo%2C%20Tokyo%2C%20Japan&place_id=ChIJu2-DAeeLGGARUZipC7OFRmA&source=structured_search_input_header&search_type=autocomplete_click', 'ginza')
+image_down('https://www.airbnb.co.uk/s/Ginza--Chuo--Tokyo--Japan/homes?tab_id=home_tab&refinement_paths%5B%5D=%2Fhomes&flexible_trip_dates%5B%5D=june&flexible_trip_dates%5B%5D=may&flexible_trip_lengths%5B%5D=weekend_trip&date_picker_type=calendar&query=Ginza%2C%20Chuo%2C%20Tokyo%2C%20Japan&place_id=ChIJu2-DAeeLGGARUZipC7OFRmA&source=structured_search_input_header&search_type=autocomplete_click', 'ginza')
